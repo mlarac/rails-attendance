ac9f6ea8976af196f3894708c3904eaa191a50e8
cklll <a418316118abc@gmail.com>
2017-02-13 02:45:34 +0800

    First commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,56 @@
+class EventsController < ApplicationController
+  def new
+    respond_to do |format|
+      format.js
+    end
+  end
+  
+  def index
+    @events = Event.all
+	respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+  
+  def edit
+    @event = Event.find(params[:id])
+	respond_to do |format|
+      format.js
+    end
+  end
+  
+  def create
+    @event = Event.new(event_params)
+    @event.save
+	
+	@events = Event.all
+    redirect_to "/events/"
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to "/events/"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to "/events/"
+  end
+  
+  def get_attendance
+    respond_to do |format|
+      msg = { :status => "ok", :message => "Success!" }
+      format.json  { render :json => msg } # don't do msg.to_json
+    end
+  end
+
+ private
+    def event_params
+      params.require(:event).permit(:name, :start_time, :end_time, :venue)
+    end
+end
---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,43 @@
+class MembersController < ApplicationController
+  def index
+    @members = Member.all
+	respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+  
+  def create
+    @member = Member.new(member_params)
+    @member.save
+	
+	@members = Member.all
+    redirect_to "/members/"
+  end
+  
+  def edit
+    @member = Member.find(params[:id])
+	respond_to do |format|
+      format.js
+    end
+  end 
+  
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to "/members/"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to "/members/"
+  end
+  
+ private
+    def member_params
+      params.require(:member).permit(:name, :serial_no)
+    end
+end
